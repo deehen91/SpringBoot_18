a9acd5dbc4cfaeb766fc83367665ce60128c7ee9
Your Name <you@example.com>
2017-10-30 15:56:03 -0400

    added registration

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,10 +1,44 @@
 package com.example.demo;
 
+import com.sun.org.apache.regexp.internal.RE;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
-public class   HomeController {
+public class HomeController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value="/register", method = RequestMethod.GET)
+    public String showRegistrationPage(Model model){
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @RequestMapping(value="/register", method = RequestMethod.POST)
+    public String processRegistrationPage(
+            @Valid @ModelAttribute("user") User user,
+            BindingResult result,
+            Model model){
+        model.addAttribute("user",user);
+        if (result.hasErrors()) {
+            return "registration";
+        } else {
+            userService.saveUser(user);
+            model.addAttribute("message",
+                    "User Account Successfully Created");
+
+        }
+        return "index";
+    }
+
     @RequestMapping("/")
     public String index(){
         return "index";
@@ -15,9 +49,10 @@ public class   HomeController {
         return "login";
     }
 
-    @RequestMapping("/admin")
-    public String admin (){
-        return "admin";
+    @RequestMapping("/secure")
+    public String secure (){
+        return "secure";
+
     }
 
 }
---
+++ src/test/java/com/example/demo/User.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+
+import javax.management.relation.Role;
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String username;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+}
